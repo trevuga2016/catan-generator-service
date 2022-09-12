dd5512a3f046f819f571ae293770e2894addb4c3
TXR3WP9 <trevor_richardson@homedepot.com>
2022-09-12 16:59:24 -0400

    swagger additions

--- src/main/java/com/generatecatanboard/controller/GeneratorController.java
+++ src/main/java/com/generatecatanboard/controller/GeneratorController.java
@@ -6,6 +6,7 @@ import com.generatecatanboard.domain.ScenarioProperties;
 import com.generatecatanboard.exceptions.InvalidBoardConfigurationException;
 import com.generatecatanboard.exceptions.PropertiesNotFoundException;
 import com.generatecatanboard.service.GeneratorService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,16 +23,19 @@ public class GeneratorController {
         this.generatorService = generatorService;
     }
 
+    @Operation(summary = "Get scenario properties", description = "Get Scenario Properties")
     @GetMapping(value = "/scenarioProps/{scenario}", produces = "application/json")
     public ScenarioProperties getScenarioProps(@PathVariable("scenario") String scenario) throws PropertiesNotFoundException, InvalidBoardConfigurationException {
         return generatorService.getScenarioProperties(scenario);
     }
 
+    @Operation(summary = "Generate a random game board", description = "Generate Random Board")
     @GetMapping(value = "/randomBoard/{scenario}", produces = "application/json")
     public BoardData generateRandomBoard(@PathVariable("scenario") String scenario, @RequestParam String harbors) throws PropertiesNotFoundException, InvalidBoardConfigurationException {
         return generatorService.generateRandomBoard(scenario, harbors);
     }
 
+    @Operation(summary = "Get the building costs for a scenario", description = "Get Building Costs")
     @GetMapping(value = "/buildingCosts/{scenario}", produces = "application/json")
     public List<BuildingCosts> getBuildingCosts(@PathVariable("scenario") String scenario) throws PropertiesNotFoundException, InvalidBoardConfigurationException {
         return generatorService.getBuildingCosts(scenario);
